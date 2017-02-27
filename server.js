0afd5179a12dd76dcb7197da10683355e7fb2800
vaibhavlall <vaibhavlall1@gmail.com>
2017-02-27 11:30:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var morgan = require('morgan');
 var path = require('path');
 var app = express();
 app.use(morgan('combined'));
+var counter=0;
+app.get('/counter',function(req,res)
+{
+  counter=counter+1;
+  res.send(counter.toString());
+});
 var articles={
 'article-one': 
 {
