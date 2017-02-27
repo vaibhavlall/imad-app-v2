c2ba5082f36ebe63bca9e1a1679176308c60969e
vaibhavlall <vaibhavlall1@gmail.com>
2017-02-27 10:25:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,3 @@
-//console.log('Loaded!');
-var a1=document.getElementById('mytext');
-a1.innerHTML='Hello New';
+console.log('Loaded!');
+document.getElementById('mytext').innerHTML='Hello New';
+//a1.innerHTML='Hello New';
