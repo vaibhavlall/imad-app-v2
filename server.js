236f9c4f6b3ea00f49ae7ad3ba0e8af373e12e7f
vaibhavlall <vaibhavlall1@gmail.com>
2017-02-28 14:03:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var config= {
     database:'db-vaibhavlall-44050',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:process.env.DB_PASSWORD
+    password:vaibhavlall
 };
 app.use(morgan('combined'));
 var counter=0;
