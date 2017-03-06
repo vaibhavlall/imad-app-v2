210f4fa8beb4eaf53fab925bf05107ba9afeb539
vaibhavlall <vaibhavlall1@gmail.com>
2017-03-06 11:56:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,16 +74,30 @@ function createTemplate(data)
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
 	var articleName=req.params.articleName;
-	res.send(createTemplate(articles[articleName]))
+pool.query("Select * from article1 where title="+req.params.articleName,function(err,result)
+{
+    if(err)
+    {
+        res.status(500).send(err.toString());
+    }
+    else
+    {
+        if(result.rows.length===0)
+        {
+            res.status(404).send('Article not Found');
+        }
+        else
+        {
+            var articleData=result.rows[0];
+         	res.send(createTemplate(articles[articleName]))
+        }
+    }
+    
+})
 });
-/*app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});*/
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
