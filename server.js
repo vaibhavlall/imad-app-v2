80c6e7646918fff7606fa294628a66402e0e5de7
vaibhavlall <vaibhavlall1@gmail.com>
2017-03-06 11:30:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/test-db',function(req,res)
         }
         else
         {
-            res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result.rows));
         }
     })
 });
