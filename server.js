8f881af2a09623480ea978b1e84a4c822ccb6ab0
vaibhavlall <vaibhavlall1@gmail.com>
2017-03-11 10:09:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var app = express();
 var Pool=require('pg').Pool;
+var crypto=require('crypto');
 
 var config= {
     user:'vaibhavlall',
@@ -45,6 +46,20 @@ function createTemplate(data)
 	<body><div class="container"><a href="/">Home</a><h1>${heading}</h1></br>${date.toDateString()}<br/><div>${content}</div></body></html>`;
 	return htmlTemplate;
 }
+
+function hash(input)
+{
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+    
+}
+app.get('/hash/:input',function(req,res)
+{
+    var hashedString=hash(req.params.input,'this-is-some-random-string');
+    res.send(hashedString);
+})
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
