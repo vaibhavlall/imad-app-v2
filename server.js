218f8cd02b1dbd53eb2bd40f48554cda5908adf6
vaibhavlall <vaibhavlall1@gmail.com>
2017-03-11 10:12:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ function createTemplate(data)
 
 function hash(input)
 {
-    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    var hashed=crypto.pbkdf2Sync(input, salt, 10000, 512,'sha512');
     return hashed.toString('hex');
     
 }
